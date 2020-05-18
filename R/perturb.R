53ea8315428ccb90328cb63cb95b36d63893cf68
Danielle Navarro <d.navarro@unsw.edu.au>
2020-05-19 07:33:05 +1000

    perturb_uniform is now a function factory

--- R/perturb.R
+++ R/perturb.R
@@ -1,17 +1,18 @@
 
 #' Perturb voronoise tiles uniformly
 #'
-#' @param data data passed from the voronoi_tile stat
 #' @param noise width of the uniform
 #'
-#' @return perturbed data
+#' @return A function that takes data as input
 #' @export
-perturb_uniform <- function(data, noise = .1) {
-  data %>%
-    dplyr::group_by(group) %>%
-    dplyr::mutate(
-      x = x + stats::runif(1, min = -noise/2, max = noise/2),
-      y = y + stats::runif(1, min = -noise/2, max = noise/2)
-    ) %>%
-    dplyr::ungroup()
+perturb_uniform <- function(noise = .1) {
+  function(data) {
+    data %>%
+      dplyr::group_by(group) %>%
+      dplyr::mutate(
+        x = x + stats::runif(1, min = -noise/2, max = noise/2),
+        y = y + stats::runif(1, min = -noise/2, max = noise/2)
+      ) %>%
+      dplyr::ungroup()
+  }
 }
