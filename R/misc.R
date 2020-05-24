c1166889dfdc759874aa08702f12667fb47a12ba
Danielle Navarro <d.navarro@unsw.edu.au>
2020-05-24 10:31:50 +1000

    don't export trivial functions

--- R/misc.R
+++ R/misc.R
@@ -1,8 +1,4 @@
 
-#' Like theme_void() but with custom background colour
-#'
-#' @param background the background colour
-#' @export
 theme_mono <- function(background = "black"){
   ggplot2::theme_void() +
   ggplot2::theme(
@@ -18,14 +14,13 @@ theme_mono <- function(background = "black"){
 }
 
 
-#' Like coord_fixed() but forced to be a square
-#'
-#' @param x bottom side of the square
-#' @param y left side of the square
-#' @param s side length
-#' @param ... arguments to be passed to coord_fixed()
-#'
-#' @export
+# #' Like coord_fixed() but forced to be a square
+# #'
+# #' @param x bottom side of the square
+# #' @param y left side of the square
+# #' @param s side length
+# #' @param ... arguments to be passed to coord_fixed()
+
 coord_square <- function(x = 0, y = 0, s = 1, ...) {
   ggplot2::coord_fixed(xlim = c(x, s), ylim = c(y, s), ...)
 }
